aef574506807bd217cbecc30bcb53da0bcf9a945
CeciLlarena <anallarena@ufm.edu>
2022-05-10 18:41:41 -0600

    insertionsort.py

--- Algorithms/Sorting/insertionsort.py
+++ Algorithms/Sorting/insertionsort.py
@@ -1,15 +1,13 @@
-def linearsearch(arr, n, x):
-    for i in range (0,n):
-        if (arr[i] == x):
-            return i
-    return -1
 
-arr = [8,9,10,11]
+def insertion_sort(arr):
 
-x = 1
-n= len(arr)
-position = linearsearch(arr, n, x)
-if (position == -1):
-    print ("element not foud")
-else:
-    print("element is present at index", position)
+     for i in range(1, len(arr)):
+  
+        key = arr[i]
+
+        j = i-1
+        while j >=0 and key < arr[j] :
+                arr[j+1] = arr[j]
+                j -= 1
+        arr[j+1] = key
+     return arr
